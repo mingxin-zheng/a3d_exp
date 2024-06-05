8617d643c2c075841299ceb8eb5fd86a5b5c5327
Mingxin Zheng <mingxinz@nvidia.com>
2024-06-05 15:05:09 +0000

    test
    Signed-off-by: Mingxin Zheng <mingxinz@nvidia.com>

--- debug.py
+++ debug.py
@@ -1,7 +1,10 @@
 import subprocess
+import sys
+from time import sleep
 
 if __name__ == "__main__":    
     # Launch the main function
-    print("Starting")
+    print("Starting", file=sys.stderr)
     subprocess.run(['bcprun', '-n', '2', '-p', '8', '-c', "python hello_world.py"])
-    print("Finished")
+    sleep(0.1)
+    print("Finished", file=sys.stderr)
