9c2e683513e18b150943e1f8df88923ad58b4493
Mingxin Zheng <mingxinz@nvidia.com>
2024-06-05 15:00:19 +0000

    test
    Signed-off-by: Mingxin Zheng <mingxinz@nvidia.com>

--- debug.py
+++ debug.py
@@ -2,4 +2,6 @@ import subprocess
 
 if __name__ == "__main__":    
     # Launch the main function
+    print("Starting")
     subprocess.run(['bcprun', '-n', '2', '-p', '8', '-c', "python hello_world.py"])
+    print("Finished")
