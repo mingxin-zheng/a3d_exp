bfbaa2a37e5738f984aeaffc4b2505208d28a1a6
Mingxin Zheng <mingxinz@nvidia.com>
2024-06-05 10:33:54 +0000

    fix
    Signed-off-by: Mingxin Zheng <mingxinz@nvidia.com>

--- debug.py
+++ debug.py
@@ -1,4 +1,7 @@
 import torch.distributed as dist
+from datetime import datetime, timedelta
+
+dist.init_process_group(backend="nccl", init_method="env://", timeout=timedelta(seconds=7200))
 
 print("hello from debug.py")
 
