afb99aa53588882d99c2ced2484b49008580dee6
Mingxin Zheng <mingxinz@nvidia.com>
2024-06-05 14:54:22 +0000

    rename
    Signed-off-by: Mingxin Zheng <mingxinz@nvidia.com>

--- debug.py
+++ debug.py
@@ -2,4 +2,4 @@ import subprocess
 
 if __name__ == "__main__":    
     # Launch the main function
-    subprocess.run(['bcprun', '-n', '2', '-p', '8', '-c', "python debug.py"])
+    subprocess.run(['bcprun', '-n', '2', '-p', '8', '-c', "python hello_world.py"])
